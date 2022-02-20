a527811056d4f9d286f595e18e8ec955057ff030
GiulioZani <giulio.zani@gmail.com>
2022-02-20 11:48:58 +0100

    all

---
+++ plot/src/main.ts
@@ -0,0 +1,51 @@
+import { Data, getPlotter } from "./utils";
+let globalIsDone = false
+const algorithmsWithColor = [['pso', 'pink'], ['ga', '#90EE90'], ['gd', 'purple']]
+for (const [alg, color] of algorithmsWithColor){
+  document.getElementById(alg)!.style.backgroundColor = color
+}
+window.plot = async () => {
+  globalIsDone = false
+  const algorithms = algorithmsWithColor.filter(a=>(document.getElementById(a[0])! as HTMLInputElement).checked)
+  if (algorithms.length > 0){
+    const indexDiv = (document.getElementById("iteration")! as HTMLInputElement);
+    indexDiv.value = `${indexDiv.value.split(':')[0]}: ${0}`
+    const objectiveFunctionName =
+      (document.getElementById("objectiveFunctionSelect")! as HTMLSelectElement)
+        .value;
+    // const fileName = 'obj_f:rosenbrock,n_par:40,iter:1000,in_srat:DynamicAdaptiveStrategy,rng:100,grd_coef:0,seed:132176,rnd_grad:false'
+    const plotters = [
+      await getPlotter(
+        algorithms.map(a=>[`./data/${objectiveFunctionName}_${a[0]}.json`, a[1]]),
+        "plot1",
+        objectiveFunctionName,
+        "surface",
+      ),
+      await getPlotter(
+        algorithms.map(a=>[`./data/${objectiveFunctionName}_${a[0]}.json`, a[1]]),
+        "plot2",
+        objectiveFunctionName,
+        "contour",
+      ),
+    ];
+    let i = 0;
+    const animate = async () => {
+      indexDiv.value = `${indexDiv.value.split(':')[0]}: ${i}`
+      let isDone = false;
+      for (const plotter of plotters) {
+        isDone = await plotter.update();
+      }
+      if (!isDone && !globalIsDone) {
+        window.requestAnimationFrame(animate);
+        i += 1;
+      }
+    };
+    window.requestAnimationFrame(animate);
+  }
+};
+
+window.stopPlot = ()=>{
+  globalIsDone = true
+}
+
+window.plot()
---
+++ plot/src/utils.ts
@@ -0,0 +1,169 @@
+class Datum {
+  position: [number, number];
+  velocity: [number, number];
+  constructor() {
+    this.position = [1, 1];
+    this.velocity = [1, 1];
+  }
+}
+type Data = [[Datum]];
+class Plotter {
+  update = async () => true;
+  constructor() {
+    this.update = async () => true;
+  }
+}
+
+const getMinPositionX = (data: Data) =>
+  Math.min(
+    ...data.flatMap((x) => x.map((y) => y.position[0])),
+  );
+const getMinPositionY = (data: Data) =>
+  Math.min(
+    ...data.flatMap((x) => x.map((y) => y.position[1])),
+  );
+const getMaxPositionX = (data: Data) =>
+  Math.max(
+    ...data.flatMap((x) => x.map((y) => y.position[0])),
+  );
+const getMaxPositionY = (data: Data) =>
+  Math.max(
+    ...data.flatMap((x) => x.map((y) => y.position[1])),
+  );
+
+const rastrigin = (x: [number, number]) =>
+  x.reduce(
+    (acc, val) => acc + Math.pow(val, 2) - 10 * Math.cos(2 * Math.PI * val),
+    0,
+  ) + 20;
+const rosenbrock = (x: [number, number]) => {
+  const a = 1;
+  const b = 100;
+  return Math.pow(a - x[0], 2) + b * Math.pow(x[1] - Math.pow(x[0], 2), 2);
+};
+
+const objectiveFunctions: Record<
+  string,
+  (c: [a: number, b: number]) => number
+> = {
+  rosenbrock,
+  rastrigin,
+};
+const dataToTrace = (dati: Datum[], type:string, objectiveFunction:(a:[number,number])=>number, color:string) => {
+    const result = {
+      x: dati.map(p=>p.position[0]),//dati.map((p) => pixDensity * normalizeY(p.position[1])),
+      y: dati.map(p=>p.position[1]),//dati.map((p) => pixDensity * normalizeX(p.position[0])),
+      type: type === "contour" ? "scatter2d" : "scatter3d",
+      mode: "markers",
+      marker: {
+        size: type === "contour" ? 10 : 2.5,
+        color: color,
+        line: {
+          width: 0.5,
+        },
+        opacity: 0.8,
+      },
+    };
+    if (type === "surface") {
+      result["z"] = dati.map((p) =>
+        objectiveFunction([p.position[1], p.position[0]])
+      );
+    }
+    return result;
+  };
+
+const getPlotter = async (
+  fileNamesWithColors: [string, string][],
+  divName: string,
+  objectiveFunctionName: string,
+  type = "contour",
+  //objectiveFunction:(c:[a:number, b:number])=>number
+): Promise<Plotter> => {
+  const objectiveFunction = objectiveFunctions[objectiveFunctionName];
+  const allData: Data[] = [];
+  const fileNames = fileNamesWithColors.map(x=>x[0])
+  for (const fileName of fileNames) {
+    const content = await (await fetch(fileName)).json() as unknown as Data;
+    allData.push(content);
+  }
+  const margin = 0.35
+  const maxPositionX = Math.max(...allData.map(data=>getMaxPositionX(data))) + margin;
+  const maxPositionY = Math.max(...allData.map(data=>getMaxPositionY(data))) + margin;
+  const minPositionX = Math.min(...allData.map(data=>getMinPositionX(data))) - margin;
+  const minPositionY = Math.min(...allData.map(data=>getMinPositionY(data))) - margin;
+  const pixDensity = 250;
+  const xStep = (maxPositionX - minPositionX) / pixDensity;
+  const yStep = (maxPositionY - minPositionY) / pixDensity;
+  const xData = []
+  const yData = []
+  const zData = [];
+  for (let i = 0; i < pixDensity; i++) {
+    const x = minPositionX + xStep * i;
+    xData.push(x)
+    const row = [];
+    for (let j = 0; j < pixDensity; j++) {
+      const y = minPositionY + yStep * j;
+      if (yData.length < pixDensity){
+        yData.push(y)
+      }
+      row.push(objectiveFunction([x, y]));
+    }
+    zData.push(row);
+  }
+  const functionTrace = {
+    z: zData,
+    y:yData,
+    x:xData,
+    opacity:0.9,
+    type: type === "contour" ? "contour" : "surface",
+  };
+  
+  const traces = [functionTrace]
+  for (let i = 0; i<allData.length; i++){
+    traces.push(dataToTrace(allData[i][0], type, objectiveFunction, fileNamesWithColors[i][1]));
+  }
+  const plot = document.getElementById(divName);
+  await Plotly.newPlot(plot, traces, {
+    width: window.innerWidth / 2.1,
+    height: window.innerHeight*0.9,
+    showlegend:false,
+    scene: {
+      camera: {
+        eye: objectiveFunctionName === "rastrigin"
+          ? { x: 0.8, y: -0.8, z: 1.5 }
+          : {},
+      },
+    },
+  });
+  const getValues = (i: number, dati: Datum[]) =>
+    dati.map(
+      (p) => p.position[i]//pixDensity * normalizeY(p.position[i]),
+    );
+
+  let i = 0;
+  const dataSize = Math.min(...allData.map(x=>x.length))
+  const plotter = {
+    "update": async () => {
+      if (i < dataSize) {
+        for (let j=0;j<allData.length; j++){
+          const trace = traces[j+1]
+          const data = allData[j]
+          trace.x = getValues(0, data[i]);
+          trace.y = getValues(1, data[i]);
+          if (type === "surface") {
+            trace["z"] = data[i].map((p) =>
+              objectiveFunction([p.position[1], p.position[0]]) + 0.01
+            );
+          }
+        }
+        await Plotly.redraw(plot);
+        i += 1;
+        return false;
+      }
+      return true;
+    },
+  };
+  return plotter as unknown as Plotter;
+};
+
+export { Data, getPlotter };
