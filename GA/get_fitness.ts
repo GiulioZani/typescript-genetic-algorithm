d3a3b3cfa62b264d002968b3ef4b0639701ebcd5
GiulioZani <giulio.zani@gmail.com>
2022-03-07 19:59:40 +0100

    windows sucks

--- GA/get_fitness.ts
+++ GA/get_fitness.ts
@@ -1,14 +1,49 @@
 import { Genome } from "./evolve.ts";
 
-let getFitness:
-  | ((a: Genome) => number)
-  | ((a: Genome) => Promise<number>)
-  | null = null;
+const getProcess = (weightsString:string) => Deno.run({
+    cmd: ["npm", "run", "get-fitness", weightsString],
+    stdout: "piped",
+    stderr: "piped",
+  });
+
+
+const getFitness = async (weights: number[], maxWait = 40000) => {
+  const weightsString = JSON.stringify(weights);
+  let code: number | null = null;
+  let p = getProcess(weightsString)
+  let startTime = performance.now()
+  const checkElapsedTime = () => {
+    if (performance.now() - startTime > maxWait){
+      console.log('Restarting subprocess')
+      p = getProcess(weightsString)
+      startTime = performance.now()
+    }
+  }
+  const interval = setInterval(checkElapsedTime, 100)
+
+  code = (await p.status()).code;
+  clearInterval(interval)
+  if (code === 0) {
+    const rawOutput = new TextDecoder().decode(await p.output());
+    //console.log(rawOutput)
+    const regex = /%(.*?)%/g;
+    const result = regex.exec(rawOutput);
+    if (result != null) {
+      const { fitness } = JSON.parse(result[1]);
+      return -fitness;
+    }
+  }
+  const rawError = new TextDecoder().decode(await p.stderrOutput());
+  console.log(rawError);
+  return Infinity;
+};
+
+
 addEventListener("message", async (event) => { // acts as an abstraction layer between the (arbitrary) evaluation function and details about thread exectution in javascript
   const parsedEvent = (event as unknown as Record<string, unknown>)["data"];
   if (typeof parsedEvent === "string") {
     try {
-      getFitness = (await import(parsedEvent)).default;
+      console.log('got one')
     } catch (e) {
       throw `Error loading module: ${e}`;
     }
